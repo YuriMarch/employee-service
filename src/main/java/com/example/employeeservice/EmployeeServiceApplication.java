abebbac388484ed91be3a3ce5e7a74299f6d256a
yurimarch <ypdmarch@gmail.com>
2022-08-23 11:25:14 +0200

    Swagger configuration added.

--- src/main/java/com/example/employeeservice/EmployeeServiceApplication.java
+++ src/main/java/com/example/employeeservice/EmployeeServiceApplication.java
@@ -1,5 +1,7 @@
 package com.example.employeeservice;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -9,5 +11,19 @@ public class EmployeeServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EmployeeServiceApplication.class, args);
+    }
+
+    @Bean
+    public OpenAPI openApiConfig(){
+        return new OpenAPI().info(apiInfo());
+    }
+
+    public Info apiInfo(){
+        Info info = new Info();
+        info.
+                title("Employee API")
+                .description("Employee API to manage employees documented using Swagger \n\n Created by Yuri March")
+                .version("v1.0.0");
+        return info;
     }
 }
--- src/main/java/com/example/employeeservice/controller/EmployeeController.java
+++ src/main/java/com/example/employeeservice/controller/EmployeeController.java
@@ -3,6 +3,9 @@ package com.example.employeeservice.controller;
 import com.example.employeeservice.model.Employee;
 import com.example.employeeservice.service.EmployeeService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -18,37 +21,49 @@ public class EmployeeController {
     private final EmployeeService employeeService;
 
     @GetMapping("/employees")
-    @Operation(summary = "Get all employees")
+    @Operation(summary = "Get all employees", responses = {
+            @ApiResponse(description = "Get all employees success ", responseCode = "200", content = @Content(mediaType = "application/json", schema= @Schema(implementation = Employee.class))),
+            @ApiResponse(description = "Bad request", responseCode = "400", content = @Content)})
     public List<Employee> getAllEmployees() {
         return employeeService.getAllEmployees();
     }
 
     @PostMapping("/employees")
-    @Operation(summary = "Save new employee")
+    @Operation(summary = "Save new employee", responses = {
+            @ApiResponse(description = "Employee successfully saved ", responseCode = "201", content = @Content(mediaType = "application/json", schema= @Schema(implementation = Employee.class))),
+            @ApiResponse(description = "Employee not saved", responseCode = "409", content = @Content)})
     public Employee saveEmployee(@RequestBody Employee employee) {
         return employeeService.saveEmployee(employee);
     }
 
     @GetMapping("/employees/{id}")
-    @Operation(summary = "Get employee by ID")
+    @Operation(summary = "Get employee by ID", responses = {
+            @ApiResponse(description = "Get employee success ", responseCode = "200", content = @Content(mediaType = "application/json", schema= @Schema(implementation = Employee.class))),
+            @ApiResponse(description = "Employee not found", responseCode = "404", content = @Content)})
     public Employee getEmployeeById(@PathVariable String id) {
         return employeeService.getEmployeeById(id);
     }
 
     @DeleteMapping("/employees/{id}")
-    @Operation(summary = "Delete employee by ID")
+    @Operation(summary = "Delete employee by ID", responses = {
+            @ApiResponse(description = "Employee successfully deleted", responseCode = "204"),
+            @ApiResponse(description = "Employee not found", responseCode = "404", content = @Content)})
     public void deleteEmployeeById(@PathVariable String id) {
         employeeService.deleteEmployeeById(id);
     }
 
     @PutMapping("/employees/{id}")
-    @Operation(summary = "Update employee by ID")
+    @Operation(summary = "Update employee by ID", responses = {
+            @ApiResponse(description = "Employee successfully updated ", responseCode = "200", content = @Content(mediaType = "application/json", schema= @Schema(implementation = Employee.class))),
+            @ApiResponse(description = "Employee not found", responseCode = "404", content = @Content)})
     public Employee updateEmployee(@PathVariable String id, @RequestBody Employee employee) {
         return employeeService.updateEmployee(id, employee);
     }
 
     @DeleteMapping("/employees")
-    @Operation(summary = "Delete all employees")
+    @Operation(summary = "Delete all employees", responses = {
+            @ApiResponse(description = "All employees successfully deleted", responseCode = "204"),
+            @ApiResponse(description = "Bad request", responseCode = "400", content = @Content)})
     public void deleteAllEmployees() {
         employeeService.deleteAllEmployees();
     }
