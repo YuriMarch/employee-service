6fc23354a3a3833bd556cfd31b65c8c559046c85
yurimarch <ypdmarch@gmail.com>
2022-08-23 10:40:48 +0200

    Swagger documentation added.

--- src/main/java/com/example/employeeservice/EmployeeServiceApplication.java
+++ src/main/java/com/example/employeeservice/EmployeeServiceApplication.java
@@ -2,6 +2,7 @@ package com.example.employeeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EmployeeServiceApplication {
@@ -9,5 +10,4 @@ public class EmployeeServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(EmployeeServiceApplication.class, args);
     }
-
 }
--- src/main/java/com/example/employeeservice/controller/EmployeeController.java
+++ src/main/java/com/example/employeeservice/controller/EmployeeController.java
@@ -2,6 +2,8 @@ package com.example.employeeservice.controller;
 
 import com.example.employeeservice.model.Employee;
 import com.example.employeeservice.service.EmployeeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,41 +12,43 @@ import java.util.List;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping
+@Tag(name="Employee")
 public class EmployeeController {
 
     private final EmployeeService employeeService;
 
-    @RequestMapping("/")
-    public String hello() {
-        return "Hello World";
-    }
-
     @GetMapping("/employees")
+    @Operation(summary = "Get all employees")
     public List<Employee> getAllEmployees() {
         return employeeService.getAllEmployees();
     }
 
     @PostMapping("/employees")
+    @Operation(summary = "Save new employee")
     public Employee saveEmployee(@RequestBody Employee employee) {
         return employeeService.saveEmployee(employee);
     }
 
     @GetMapping("/employees/{id}")
+    @Operation(summary = "Get employee by ID")
     public Employee getEmployeeById(@PathVariable String id) {
         return employeeService.getEmployeeById(id);
     }
 
     @DeleteMapping("/employees/{id}")
+    @Operation(summary = "Delete employee by ID")
     public void deleteEmployeeById(@PathVariable String id) {
         employeeService.deleteEmployeeById(id);
     }
 
     @PutMapping("/employees/{id}")
+    @Operation(summary = "Update employee by ID")
     public Employee updateEmployee(@PathVariable String id, @RequestBody Employee employee) {
         return employeeService.updateEmployee(id, employee);
     }
 
     @DeleteMapping("/employees")
+    @Operation(summary = "Delete all employees")
     public void deleteAllEmployees() {
         employeeService.deleteAllEmployees();
     }
